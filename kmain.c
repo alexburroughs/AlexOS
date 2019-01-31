c0c8cd94aebe0ee3a69a2a25451fd5a347d62df5
Alex Burroughs <aburroughs00@gmail.com>
2019-01-31 13:51:25 -0500

    write to screen

--- kmain.c
+++ kmain.c
@@ -1,3 +1,12 @@
-void kmain() {
+void fp_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
 
+void kmain() {
+	fp_write_cell(0, 'A', 2, 8);
+}
+
+void fp_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg) {
+
+	char *fb = (char *) 0x000B8010;	
+	fb[i] = c;
+	fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
 }
